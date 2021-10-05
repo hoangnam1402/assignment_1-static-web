b1f31066f575bcd4e49a84685aea2563ccc2545c
hoangnam1402 <18521130@gm.uit.edu.vn>
2021-10-06 00:27:36 +0700

    sort

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let list = document.querySelectorAll(".list");
+let itemBox = document.querySelectorAll(".card");
 
 for(let i=0;i<list.length;i++) {
     list[i].addEventListener("click",function(){
@@ -6,6 +7,18 @@ for(let i=0;i<list.length;i++) {
             list[j].classList.remove("active");
         }
         this.classList.add("active");
+        
+        let dataFilter = this.getAttribute("data-filter");
+        for (let k = 0; k < itemBox.length; k++) {
+        itemBox[k].classList.remove("active");
+        itemBox[k].classList.add("hide");
+        console.log(dataFilter, itemBox[k].getAttribute("data-item"));
+        if (itemBox[k].getAttribute("data-item") == dataFilter ||
+            dataFilter == "All") {
+            itemBox[k].classList.remove("hide");
+            itemBox[k].classList.add("active");
+        }
+      }
     })
 }
 const buttonsPagination = document.querySelectorAll(".pagination > button");
