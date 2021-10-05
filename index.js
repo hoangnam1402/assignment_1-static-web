ac69c2d95a3d6f05fee048b3ba6b0bb0cca7390b
hoangnam1402 <18521130@gm.uit.edu.vn>
2021-10-06 00:09:34 +0700

    Pagination 3

--- index.js
+++ index.js
@@ -10,23 +10,11 @@ for(let i=0;i<list.length;i++) {
 }
 const buttonsPagination = document.querySelectorAll(".pagination > button");
 
-let now =  1;
 for (let i = 0; i < buttonsPagination.length; i++) {
-    if(i = 0 && now > 1){
-        buttonsPagination[i].addEventListener("click", function () {
-            for (let j = 0; j < buttonsPagination.length; j++) {
-                buttonsPagination[j].classList.remove("active");
-            }
-            buttonsPagination[now-1].classList.add("active");
-        })
-    }
-    else
-        {buttonsPagination[i].addEventListener("click", function () {
-            for (let j = 0; j < buttonsPagination.length; j++) {
-                buttonsPagination[j].classList.remove("active");
-            }
-            this.classList.add("active");
-            now =  i;
-        })
-    }
+    buttonsPagination[i].addEventListener("click", function () {
+        for (let j = 0; j < buttonsPagination.length; j++) {
+            buttonsPagination[j].classList.remove("active");
+        }
+        this.classList.add("active");
+    })
 }
